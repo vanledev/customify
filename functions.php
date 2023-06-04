a4a325c22bea435c9f7bc276ee7a815932eb7576
moe <you@example.com>
2023-06-05 06:08:41 +0700

    add category image for Post Categories #317

--- functions.php
+++ functions.php
@@ -86,3 +86,51 @@ function Customify() {
 
 Customify();
 
+
+//add extra fields to category edit form hook
+add_action ( 'edit_category_form_fields', 'extra_category_fields');
+
+//add extra fields to category edit form callback function
+function extra_category_fields( $tag ) {    //check for existing featured ID
+    $t_id = $tag->term_id;
+    $cat_meta = get_option( "category_$t_id");
+	if (isset($cat_meta['img'])){
+		$cat_meta_img=$cat_meta['img'];
+	}  else {
+$cat_meta_img = '';
+	}
+	
+?>
+<tr class="form-field">
+<th scope="row" valign="top"><label for="cat_Image_url"><?php _e('Category Image Url'); ?></label></th>
+<td>
+    <img src="<?php echo $cat_meta_img ; ?>" />
+<input type="text" name="Cat_meta[img]" id="Cat_meta[img]" size="3" style="width:60%;" value="<?php echo $cat_meta_img ; ?>"><br />
+        <span class="description"><?php _e('Insert full URL of the image here'); ?></span>
+</tr>
+
+
+
+<?php
+} 
+
+
+wp_enqueue_script( 'myjs', get_template_directory_uri() .  '/assets/js/post-category.js' ,['jquery']);
+// save extra category extra fields hook
+add_action ( 'edited_category', 'save_extra_category_fileds');
+
+// save extra category extra fields callback function
+function save_extra_category_fileds( $term_id ) {
+    if ( isset( $_POST['Cat_meta'] ) ) {
+        $t_id = $term_id;
+        $cat_meta = get_option( "category_$t_id");
+        $cat_keys = array_keys($_POST['Cat_meta']);
+            foreach ($cat_keys as $key){
+            if (isset($_POST['Cat_meta'][$key])){
+                $cat_meta[$key] = $_POST['Cat_meta'][$key];
+            }
+        }
+        //save the option array
+        update_option( "category_$t_id", $cat_meta );
+    }
+}
